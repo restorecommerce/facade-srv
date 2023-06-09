330debb5ca1f4e04ae03cf57a4f59ef90a6c81f2
akumar <arun.kumar@n-fuse.co>
2023-06-09 17:30:39 +0200

    refactor: move facade-srv to esm

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  "root": false,
+  "parserOptions": {
+    "project": [
+      "tsconfig.lib.json",
+      "tsconfig.test.json",
+      "tsconfig.debug.json",
+      "tsconfig.codegen.json",
+      "tsconfig.generate.json"
+    ],
+    "sourceType": "module",
+    "ecmaVersion": 2020
+  }
+};
