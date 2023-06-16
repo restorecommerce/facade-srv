7ca099bddf180299d3bbc043501475998ac6c923
akumar <arun.kumar@n-fuse.co>
2023-06-16 12:27:40 +0200

    chore: up facade (file upload and download)

--- test/worker.ts
+++ test/worker.ts
@@ -2,8 +2,8 @@ import { Worker } from '../src/worker.js';
 import { createServiceConfig } from '@restorecommerce/service-config';
 import { createLogger } from '@restorecommerce/logger';
 
-export function createTestWorker() {
+export const createTestWorker = () => {
   const cfg = createServiceConfig(process.cwd() + '/test');
   const logger = createLogger(cfg.get('logger'));
   return new Worker(cfg, logger);
-}
+};
