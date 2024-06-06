34f548ff7c0cfaa89f3ac229d559c4b05adc14b7
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2024-06-06 12:26:28 +0200

    chore: up RC deps and docker node version, log w/o JSON stringify

--- src/start.ts
+++ src/start.ts
@@ -5,10 +5,6 @@ import { createLogger } from '@restorecommerce/logger';
 const startServer = () => {
   const cfg = createServiceConfig(process.cwd());
   const loggerCfg = cfg.get('logger');
-  loggerCfg.esTransformer = (msg: any) => {
-    msg.fields = JSON.stringify(msg.fields);
-    return msg;
-  };
   const logger = createLogger(loggerCfg);
 
   try {
